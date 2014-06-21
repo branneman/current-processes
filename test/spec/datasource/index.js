01a848676726e100907926f3d04b87f290071c52
branneman <branmovic@gmail.com>
2014-06-22 01:11:29 +0200

    Removed redundant, nested describe() in DataSource test

--- test/spec/datasource/index.js
+++ test/spec/datasource/index.js
@@ -5,56 +5,53 @@ var rewire = require('rewire');
 
 describe('DataSource', function() {
 
-    describe('Method get()', function() {
+    it('should not fail when require()\'d', function() {
 
-        it('should not fail when require()\'d', function() {
+        var DataSource = require('../../../lib/datasource');
 
-            var DataSource = require('../../../lib/datasource');
+        assert(DataSource);
+    });
 
-            assert(DataSource);
-        });
+    it('should have a get() method', function() {
 
-        it('should have a get() method', function() {
+        var DataSource = require('../../../lib/datasource');
 
-            var DataSource = require('../../../lib/datasource');
+        assert.isFunction(DataSource.get);
+    });
 
-            assert.isFunction(DataSource.get);
-        });
+    it('should return the right module for each platform', function() {
 
-        it('should return the right module for each platform', function() {
+        var fixtures = [
+            {
+                platform: 'darwin',
+                function: 'dataSourcePS'
+            },
+            {
+                platform: 'freebsd',
+                function: 'dataSourcePS'
+            },
+            {
+                platform: 'linux',
+                function: 'dataSourcePS'
+            },
+            {
+                platform: 'sunos',
+                function: 'dataSourcePS'
+            },
+            {
+                platform: 'win32',
+                function: 'dataSourceWMIC'
+            }
+        ];
 
-            var fixtures = [
-                {
-                    platform: 'darwin',
-                    function: 'dataSourcePS'
-                },
-                {
-                    platform: 'freebsd',
-                    function: 'dataSourcePS'
-                },
-                {
-                    platform: 'linux',
-                    function: 'dataSourcePS'
-                },
-                {
-                    platform: 'sunos',
-                    function: 'dataSourcePS'
-                },
-                {
-                    platform: 'win32',
-                    function: 'dataSourceWMIC'
-                }
-            ];
+        fixtures.forEach(function(fixture) {
 
-            fixtures.forEach(function(fixture) {
+            var DataSource = rewire('../../../lib/datasource');
+            DataSource.__set__('platform', fixture.platform);
 
-                var DataSource = rewire('../../../lib/datasource');
-                DataSource.__set__('platform', fixture.platform);
+            var result = DataSource.get();
 
-                var result = DataSource.get();
-
-                assert.equal(result.name, fixture.function);
-            });
+            assert.equal(result.name, fixture.function);
         });
     });
 });
